0af3069999a5fc85dc69787b2033733ad49b61c2
Marta Grazia <airwa90@gmail.com>
2023-03-25 21:50:43 +0000

    added basic functions + piece randomization

--- script.js
+++ script.js
@@ -1,40 +1,88 @@
-const possiblePieces  = ["t", "i", "s", "l"];
-startPiece("s")
+const possiblePieces  = ["t", "i", "s", "l"]; //array with the possible piece shapes
+let currentPiece = pieceSelect(possiblePieces); //starting shape selection
+startPiece(currentPiece);   //startin shape display
 
+
+//event listener to detect which arrow key is pressed
+document.addEventListener('keydown', (e) => {
+    e = e || window.event;
+    if (e.key === 'ArrowUp') {
+        pieceRotationClock();
+    }
+    if(e.key === 'ArrowDown') {
+        pieceRotationCounter();
+    }
+    if(e.key === 'ArrowLeft') {
+        pieceMoveLeft();
+    }
+    if (e.key === 'ArrowRight') {
+        pieceMoveRight();
+    }
+    if (e.key === ' '){
+        pieceDropDown();
+    }
+    
+})
+
+
+//function to display the first piece once randomly detected
 function startPiece(shape) {
     switch (shape) {
         case "t":
-            highlightCell("d1d");
-            highlightCell("d2c");
-            highlightCell("d2d");
-            highlightCell("d3d");
+            highlightCell("1d");
+            highlightCell("2c");
+            highlightCell("2d");
+            highlightCell("3d");
             break;
         case "i":
-            highlightCell("d1d");
-            highlightCell("d4d");
-            highlightCell("d2d");
-            highlightCell("d3d");
+            highlightCell("1d");
+            highlightCell("4d");
+            highlightCell("2d");
+            highlightCell("3d");
             break;
         case "l":
-            highlightCell("d1c");
-            highlightCell("d2c");
-            highlightCell("d3c");
-            highlightCell("d3d");
+            highlightCell("1c");
+            highlightCell("2c");
+            highlightCell("3c");
+            highlightCell("3d");
             break;
         case "s":
-            highlightCell("d1c");
-            highlightCell("d2c");
-            highlightCell("d2d");
-            highlightCell("d3d");
-            break;
-            
+            highlightCell("1c");
+            highlightCell("2c");
+            highlightCell("2d");
+            highlightCell("3d");
+            break;            
     }
-
 }
 
+//funcion to change the color of a cell to highlight the piece shape
 function highlightCell(id) {
     document.getElementById(id).style.background = "chartreuse";
 }
+//function to randomly pick aa shape from the array
+function pieceSelect(arr){
+    let index;
+    index = Math.floor(Math.random() * arr.length);
+    return arr[index]
+}
 
+function pieceRotationClock() {
 
+}
 
+function pieceRotationCounter(){
+
+}
+function pieceMoveLeft(){
+
+}
+function pieceMoveRight(){
+
+}
+function pieceDropDown() {
+
+}
+
+function pieceGravity(){
+
+}
